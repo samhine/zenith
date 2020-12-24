c804ee89c1f46b91b38ee9fb7c2432938cfe20c5
u1615413 <sam.hine27@gmail.com>
2020-12-24 16:25:49 +0000

    rate limit vars, ready for implementation

--- app/util.js
+++ app/util.js
@@ -8,11 +8,18 @@
  * @return JSON representation of Riot API response.
  */
 
+
+ const SHORT_CALL_LIMIT = 20;
+ const SHORT_CALL_PERIOD = 1;
+ const LONG_CALL_LIMIT = 100;
+ const LONG_CALL_PERIOD = 120;
+
 function makeRiotApiCall(region, query, secret){
     if (query.endsWith("&") || query.endsWith("?")) var url = "https://"+region+".api.riotgames.com"+query+"api_key="+secret;
     else var url = "https://"+region+".api.riotgames.com"+query+"?api_key="+secret;
 
-    var response = UrlFetchApp.fetch(url, {'muteHttpExceptions': true});
+
+    var response = UrlFetchApp.fetch(url, {'muteHttpExceptions': false});
     var json = response.getContentText();
     var data = JSON.parse(json);
     return data
